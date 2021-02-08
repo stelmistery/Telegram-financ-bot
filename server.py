fa508da781643cf24860e4a676d9469c1c779a2d
Vladislav <levelup996@gmail.com>
2021-02-08 15:06:43 +0300

    inital project

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 """Сервер Telegram бота, запускаемый непосредственно"""
+
 import logging
 import os
-
+from decouple import config
 import aiohttp
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -13,15 +14,15 @@ from middlewares import AccessMiddleware
 
 logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
-PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
-PROXY_AUTH = aiohttp.BasicAuth(
-    login=os.getenv("TELEGRAM_PROXY_LOGIN"),
-    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
-)
-ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
+API_TOKEN = config("API_TOKEN")
+# PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
+# PROXY_AUTH = aiohttp.BasicAuth(
+#     login=os.getenv("TELEGRAM_PROXY_LOGIN"),
+#     password=os.getenv("TELEGRAM_PROXY_PASSWORD")
+# )
+ACCESS_ID = config("TELEGRAM_ACCESS_ID")
 
-bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(AccessMiddleware(ACCESS_ID))
 
